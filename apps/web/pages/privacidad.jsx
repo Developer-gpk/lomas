1378aeda4d945499facc83a0f992b5690946e714
Ruben Trujillo <70417905+RubenElyTrujillo@users.noreply.github.com>
2022-11-04 16:23:27 -0600

    privacidad

--- apps/web/pages/privacidad.jsx
+++ apps/web/pages/privacidad.jsx
@@ -4,7 +4,179 @@ import Layout from '../src/Layout/Layout'
 export default function Privacidad(){
     return(
         <Layout title={"Aviso de privacidad | Lomas Home"}>
-
+            <div className='privacidad'>
+                <div className='holder'>
+                    <div className="container-fluid">
+                        <h1>AVISO DE PRIVACIDAD</h1>
+                        <p>
+                            De conformidad con lo establecido en la Ley Federal de Protección de Datos 
+                            Personales en Posesión de los Particulares, <strong>VARALTA, S.A. DE C.V. - LOMAS HOME </strong> 
+                            pone a su disposición el siguiente aviso de privacidad.
+                        </p>
+                        <p>
+                            <strong>VARALTA, S.A. DE C.V. - LOMAS HOME,</strong> es responsable del uso y protección de sus 
+                            datos personales, en este sentido y atendiendo las obligaciones legales establecidas en 
+                            la Ley Federal de Protección de Datos Personales en Posesión de los Particulares, a 
+                            través de este instrumento se informa a los titulares de los datos, la información que de 
+                            ellos se recaba y los fines que se le darán a dicha información.
+                        </p>
+                        <p>
+                            Además de lo anterior, informamos a usted que <strong> VARALTA, S.A. DE C.V. - LOMAS 
+                            HOME,</strong> tiene su domicilio ubicado en:
+                        </p>
+                        <p>
+                            Calle Niños Héroes 4927-2, Col. San Miguel La Rosa, C.P. 72190, Puebla, Pue.
+                        </p>
+                        <p>
+                            Los datos personales que recabamos de usted serán utilizados para las siguientes 
+                            finalidades, las cuales son necesarias para concretar nuestra relación con usted, así 
+                            como para atender los servicios y/o pedidos que solicite:
+                        </p>
+                        <p>
+                            El objeto principal de la sociedad será:
+                        </p>
+                        <p>
+                            1.Compra, venta, arrendamiento y subarrendamiento de toda clase de bienes 
+                            inmuebles.
+                        </p>
+                        <p>
+                            2.Compra, venta, arrendamiento, importación, fabricación de bienes muebles.
+                        </p>
+                        <p>
+                            3.Construcción y enajenación de desarrollos inmobiliarios, promoción, compra y 
+                            venta de terrenos, casas habitación, centros y locales comerciales, edificios, 
+                            bodegas, fraccionamientos, desarrollos turísticos y toda clase de bienes inmuebles, 
+                            a través de sistemas tradicionales de mercadeo o bien por internet, call center o 
+                            cualquier otro medio.
+                        </p>
+                        <p>
+                            4.Comisiones, consignaciones y representaciones, relacionadas con la industria.
+                        </p>
+                        <p>
+                            5. La celebración de todos los actos, contratos y operciones anexos, conexos o que 
+                            se relacionen con los objetos especificados y que sean necesarios o convenientes 
+                            para la realización del objeto social.
+                        </p>
+                        <p>
+                            6. Los demás negocios derivados o conexos que de común acuerdo convengan los 
+                            socios y no sean contrarios a las Leyes de los Estados Unidos Mexicanos. 
+                        </p>
+                        <p>
+                            Para llevar a cabo las finalidades descritas en el presente aviso de privacidad, 
+                            utilizaremos los siguientes datos personales:
+                        </p>
+                        <p>
+                            Nombre, teléfono, dirección, correo electrónico, bien inmueble de interés, 
+                            Identificación personal, acta constitutiva escritura, prediales, recibos de pagos de 
+                            servicios, reporte de gravámenes.
+                        </p>
+                        <p>
+                            Por otra parte, informamos a usted, que sus datos personales no serán compartidos con 
+                            ninguna autoridad, empresa, organización o persona distintas a nosotros y serán 
+                            utilizados exclusivamente para los fines señalados.
+                        </p>
+                        <p>
+                            Usted tiene en todo momento el derecho a conocer qué datos personales tenemos de 
+                            usted, para qué los utilizamos y las condiciones del uso que les damos (Acceso). 
+                            Asimismo, es su derecho solicitar la corrección de su información personal en caso de 
+                            que esté desactualizada, sea inexacta o incompleta (Rectificación); de igual manera, 
+                            tiene derecho a que su información se elimine de nuestros registros o bases de datos 
+                            cuando considere que la misma no está siendo utilizada adecuadamente (Cancelación); 
+                            así como también a oponerse al uso de sus datos personales para fines específicos 
+                            (Oposición). Estos derechos se conocen como derechos ARCO.
+                        </p>
+                        <p>
+                            Para el ejercicio de cualquiera de los derechos ARCO, se deberá presentar la solicitud 
+                            respectiva a través de los formatos que estarán a su disposición en:
+                        </p>
+                        <p>
+                            www.lomashome.com.mx, redes sociales, o el las oficinas en la calle Niños Héroes 
+                            4927-2, Col. San Miguel La Rosa, C.P. 72190, en Puebla, Pue., o por los teléfonos 
+                            de la empresa.
+                        </p>
+                        <p>
+                            Lo anterior también servirá para conocer el procedimiento y requisitos para el ejercicio 
+                            de los derechos ARCO, no obstante, la solicitud de ejercicio de estos derechos debe 
+                            contener la siguiente información:
+                        </p>
+                        <p>
+                            Nombre, teléfono, dirección, correo electrónico, bien inmueble de interés, 
+                            Identificación personal, acta constitutiva escritura, prediales, recibos de pagos de 
+                            servicios, reporte de gravámenes.
+                        </p>
+                        <p>
+                            La respuesta a la solicitud se dará en el siguiente plazo: 3 días hábiles, y se comunicará 
+                            de la siguiente manera:
+                        </p>
+                        <p>
+                            A través de correo electrónico, vía telefónica incluido WhatsApp o alguna aplicación 
+                            similar
+                        </p>
+                        <p>
+                            Los datos de contacto de la persona o departamento de datos personales, que está a 
+                            cargo de dar trámite a las solicitudes de derechos ARCO, son los siguientes:
+                        </p>
+                        <p>a) Nombre del responsable: Departamento de promoción</p>
+                        <p>b) Domicilio:</p>
+                        <p>Niños Héroes 4927-2, Col. San Miguel La Rosa, C.P. 72190, Puebla, Pue.</p>
+                        <p>c) Teléfono: 222-6886134 y 221-3634540</p>
+                        <p>d) Correo electrónico: contacto@lomashome.com.mx</p>
+                        <p>
+                            Cabe mencionar, que en cualquier momento usted puede revocar su consentimiento 
+                            para el uso de sus datos personales. Del mismo modo, usted puede revocar el 
+                            consentimiento que, en su caso, nos haya otorgado para el tratamiento de sus datos 
+                            personales. Sin embargo, es importante que tenga en cuenta que no en todos los casos 
+                            podremos atender su solicitud o concluir el uso de forma inmediata, ya que es posible 
+                            que por alguna obligación legal se requiera seguir tratando sus datos personales. 
+                            Asimismo, usted deberá considerar que para ciertos fines la revocación de su 
+                            consentimiento implicará que no podamos seguir prestando el servicio que nos solicitó, 
+                            o la conclusión de su relación con nosotros.
+                        </p>
+                        <p>
+                            Para revocar el consentimiento que usted otorga en este acto o para limitar su 
+                            divulgación, se deberá presentar la solicitud respectiva por escrito, mediante el envío de 
+                            una carta o solicitud en formato libre a la siguiente dirección:
+                        </p>
+                        <p>
+                            Niños Héroes 4927-2, Col. San Miguel La Rosa, C.P. 72190, Puebla, Pue.
+                        </p>
+                        <p>
+                            Del mismo modo, podrá solicitar la información para conocer el procedimiento y 
+                            requisitos para la revocación del consentimiento, así como limitar el uso y divulgación 
+                            Page 4 of 4
+                            de su información personal, sin embargo, estas solicitudes deberán contener la 
+                            siguiente información:
+                        </p>
+                        <p>
+                            Nombre, teléfono, correo electrónico.
+                        </p>
+                        <p>
+                            La respuesta a la solicitud de revocación o limitación de divulgación de sus datos se 
+                            dará a más tardar en el siguiente plazo: 7 días hábiles, y se comunicará de la siguiente 
+                            forma:
+                        </p>
+                        <p>
+                            A través de correo electrónico o carta membreteada de la empresa
+                        </p>
+                        <p>
+                            El presente aviso de privacidad puede sufrir modificaciones, cambios o actualizaciones 
+                            derivadas de nuevos requerimientos legales; de nuestras propias necesidades por los 
+                            productos o servicios que ofrecemos; de nuestras prácticas de privacidad; de cambios 
+                            en nuestro modelo de negocio, o por otras causas, por lo cual, nos comprometemos a 
+                            mantenerlo informado sobre los cambios que pueda sufrir el presente aviso de 
+                            privacidad, sin embargo, usted puede solicitar información sobre si el mismo ha sufrido 
+                            algún cambio a través del siguiente correo electrónico:
+                        </p>
+                        <p>
+                            contacto@lomashome.com.mx
+                        </p>
+                        <p>
+                        Última actualización: <br />
+                        04/11/2022
+                        </p>
+                    </div>
+                </div>
+            </div>
         </Layout>
     )
 }
--- apps/web/src/Components/Footer/Footer.jsx
+++ apps/web/src/Components/Footer/Footer.jsx
@@ -66,7 +66,9 @@ export default function Footer(){
                         <div className='col-md-3 derechos'>
                             <span>Lomas home © 2022.</span>
                             <span>Todos los derechos reservados.</span><br/>
-                            <a href="/AVISO DE PRIVACIDAD LOMAS HOME.pdf" target="_bank" style={{ textDecoration: "none", color: "#FFFFFF"}}><span>AVISO DE PRIVACIDAD</span></a>
+                            <Link href="/privacidad">
+                                <a target="_bank" style={{ textDecoration: "none", color: "#FFFFFF"}}><span>AVISO DE PRIVACIDAD</span></a>
+                            </Link>
                         </div>
                         <div className='col-md-7 col-4 centro'>
                             <ul className="nav justify-content-start">
