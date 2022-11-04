ae5d911ada1a94f8a59e7b677acb35baa8b67326
Ruben Trujillo <70417905+RubenElyTrujillo@users.noreply.github.com>
2022-11-03 18:04:48 -0600

    privacidad

---
+++ apps/web/pages/privacidad.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../src/Layout/Layout'
+
+export default function Privacidad(){
+    return(
+        <Layout title={"Aviso de privacidad | Lomas Home"}>
+
+        </Layout>
+    )
+}
